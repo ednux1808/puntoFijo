ac8d9371196f418b5a024aca256572fdd3a0d64b
Eduardo Zamora <esdu1808@gmail.com>
2019-01-25 14:43:32 -0600

    v0.0.1a

---
+++ FixedPoint/src/java/controll/controlApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controll;
+
+import modell.Operation;
+import modell.library.MathKit;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class controlApp {
+    public static void main(String[] args) {
+        Operation app = new Operation();
+        MathKit library = new MathKit();
+        library.evaluar();
+    }
+}
